b8bb0790ea02f26888c005b0f736fed7c15664a5
Cristian Chazarreta <cristianchazarreta23.com>
2023-05-02 20:23:20 -0300

    cambios en el carrito

--- app.js
+++ app.js
@@ -1,66 +1,109 @@
-const producto = [
-{ nombre: "matafuego", precio: 1500},
+const informacionCompra = document.getElementById('informacionCompra');
+const contenedorCompra = document.getElementById('contenedorCompra');
+const productosCompra = document.getElementById('productosCompra');
+const contenedor = document.getElementById('contenedor');
+const carrito = document.getElementById('carrito');
+const numero = document.getElementById("numero");
+const header = document.querySelector("#header");
+const total = document.getElementById('total');
+const body = document.querySelector("body");
+const x = document.getElementById('x')
 
-{ nombre: "manguera", precio: 150000},
+// Variables que vamos a usar en nuestoro proyecto
+let lista = []
+let valortotal = 0
 
-{ nombre: "cinta", precio: 5000},
-
-{ nombre: "matafuego 5kg", precio: 28500}
-
-];
-
-let carrito = []
-
-let seleccion = prompt("¿Queres realizar una compra?")
-
-while(seleccion != "si" && seleccion != "no"){
-alert("por favor ingresa si o no")
-seleccion = prompt("¿Queres visitar la Tienda Online?") 
-}
+// Scroll de nuestra pagina
+window.addEventListener("scroll", function () {
+    if (contenedor.getBoundingClientRect().top < 10) {
+        header.classList.add("scroll")
+    }
+    else {
+        header.classList.remove("scroll")
+    }
+})
 
 
-if(seleccion == "si"){
-    alert("Te estas dirigiendo a nuestra lista de productos")
-    let todoslosProductos = producto.map(
-        (producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todoslosProductos.join( " - "))
-}
-else if (seleccion == "no"){
-alert ("Está bien, no vuelvas más.")}
+window.addEventListener('load', () => {
+    visualizarProductos();
+    contenedorCompra.classList.add("none")
+})
 
-while(seleccion != "no"){
-    let producto = prompt("Agrega un producto al carrito!")
-    let precio = 0 
-
-    if(producto == "matafuego" || producto == "matafuego 5kg" || producto == "cinta" || producto == "manguera") {
-        switch(producto){
-            case "matafuego":
-            precio = 18500;
-            break;
-
-            case "matafuego 5kg ":
-            precio = 58500;
-            break;
-
-            case "Cinta":
-            precio = 1500;
-            break;
-
-            case "Manguera":
-            precio = 180000;
-            break;
+function visualizarProductos() {
+    contenedor.innerHTML = ""
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].existencia > 0) {
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
         }
-        let unidades = parseInt(prompt("¿Cuántas Unidades vas a llevar?"))
-        carrito.push({producto,unidades,precio})
-        console.log(carrito)
-        } 
-        seleccion = prompt("¿Llevas algo más?")
-
-        while(seleccion == "no"){
-            alert("Gracias por la compra!")
-            carrito.forEach((carritoFinal) => {
-                console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar ${ carritoFinal.unidades * carritoFinal.precio}`)
-            })
-            break;
+        else {
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
         }
+    }
 }
+
+function comprar(indice) {
+    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
+
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == productos[indice].nombre) {
+            productos[i].existencia -= 1
+            if (productos[i].existencia == 0) {
+                visualizarProductos()
+            }
+            van = false
+        }
+        guardarAlmacenamientoLocal("productos", productos)
+        i += 1
+    }
+    numero.innerHTML = lista.length
+    numero.classList.add("diseñoNumero")
+    return lista
+}
+
+carrito.addEventListener("click", function(){
+    body.style.overflow = "hidden"
+    contenedorCompra.classList.remove('none')
+    contenedorCompra.classList.add('contenedorCompra')
+    informacionCompra.classList.add('informacionCompra')
+    mostrarElemtrosLista()
+})
+
+function mostrarElemtrosLista() {
+    productosCompra.innerHTML = ""
+    valortotal = 0
+    for (let i = 0; i < lista.length; i++){
+        productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`
+        valortotal += parseInt(lista[i].precio)
+    }
+    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal}</span></p>`
+}
+
+function eliminar(indice){
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == lista[indice].nombre) {
+            productos[i].existencia += 1
+            lista.splice(indice, 1)
+            van = false
+        }
+        i += 1
+    }
+    guardarAlmacenamientoLocal("productos", productos)
+
+    numero.innerHTML = lista.length
+    if (lista.length == 0){
+        numero.classList.remove("diseñoNumero")
+    }
+    visualizarProductos()
+    mostrarElemtrosLista()
+}
+
+x.addEventListener("click", function(){
+    body.style.overflow = "auto"
+    contenedorCompra.classList.add('none')
+    contenedorCompra.classList.remove('contenedorCompra')
+    informacionCompra.classList.remove('informacionCompra')
+})
