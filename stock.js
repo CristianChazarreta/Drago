065aed1b1cfe1e6cb092b6cc7d3200abfb5fa842
Cristian Chazarreta <cristianchazarreta23.com>
2023-06-08 17:45:01 -0300

    carga de img2

--- stock.js
+++ stock.js
@@ -1,7 +1,7 @@
 
 let stockProductos = [
-    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 120000, img: './img2/dragoimg1.jpg'},
-    {id: 2, nombre: "Matafuegos 1kg", tipo: "1kg", cantidad: 1, desc: "Matafuegos de 1kg ideales para automotores, y embarcaciones.", precio: 80000,  img: './img2/mata5k.jpg'},
-    {id: 3, nombre: "Valvula", tipo: "N/A", cantidad: 1, desc: "Valvula de seguridad para sistemas de prevención.", precio: 1200, talle: "M", img: './img2/valvula.jpg'},
+    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 120000, img: '../img2/dragoimg1.jpg'},
+    {id: 2, nombre: "Matafuegos 1kg", tipo: "1kg", cantidad: 1, desc: "Matafuegos de 1kg ideales para automotores, y embarcaciones.", precio: 80000,  img: '../img2/mata5k.jpg'},
+    {id: 3, nombre: "Valvula", tipo: "N/A", cantidad: 1, desc: "Valvula de seguridad para sistemas de prevención.", precio: 1200, talle: "M", img: '../img2/valvula.jpg'},
 
 ]
