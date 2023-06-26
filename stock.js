6ac08e620f7a78869822901072861e0adba03a4e
Cristian Chazarreta <cristianchazarreta23.com>
2023-06-26 10:37:47 -0300

    actualización de estilos

--- stock.js
+++ stock.js
@@ -1,7 +1,12 @@
 
 let stockProductos = [
-    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 120000, img: '../img2/dragoimg1.jpg'},
-    {id: 2, nombre: "Matafuegos 1kg", tipo: "1kg", cantidad: 1, desc: "Matafuegos de 1kg ideales para automotores, y embarcaciones.", precio: 80000,  img: '../img2/mata5k.jpg'},
-    {id: 3, nombre: "Valvula", tipo: "N/A", cantidad: 1, desc: "Valvula de seguridad para sistemas de prevención.", precio: 1200, talle: "M", img: '../img2/valvula.jpg'},
+    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 25000, img: '../img2/5kg'},
+    {id: 2, nombre: "Matafuegos 1kg", tipo: "1kg", cantidad: 1, desc: "Matafuegos de 1kg ideales para automotores, y embarcaciones.", precio: 80000,  img: '../img2/1kg.jpg'},
+    {id: 3, nombre: "Manguera", tipo: "N/A", cantidad: 1, desc: "Manguera de seguridad para sistemas de prevención.", precio: 1200,  img: '../img2/manguera.jpg'},
+    {id: 4, nombre: "Cinta Doble Faz", tipo: "1U", cantidad: 1, desc: "Cinta doble faz para todo tipo de uso.", precio: 800,  img: '../img2/cintadoble.jpg'},
+    {id: 5, nombre: "Cartelería", tipo: "1U", cantidad: 1, desc: "Cartelería de todo tipo, consulte variedad.", precio: 800,  img: '../img2/carteleria.jpg'},
+    {id: 6, nombre: "Gabinete para Extintor", tipo: "N/A", cantidad: 1, desc: "Gabinete para extintor 5kg, sobre pared.", precio: 15000, talle: "M", img: '../img2/gabinete.jpg'},
+    {id: 7, nombre: "Luz de Emergencia", tipo: "1U", cantidad: 1, desc: "Luces de emergencias automáticas duración 15 horas.", precio: 120000, img: '../img2/emergencias.jpg'},
+  
 
 ]
