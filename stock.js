69f22b24b870371310c5a41d90c2c941d9261dfb
Cristian Chazarreta <cristianchazarreta23.com>
2023-06-26 10:39:10 -0300

    carga de productos nuevos

--- stock.js
+++ stock.js
@@ -1,6 +1,6 @@
 
 let stockProductos = [
-    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 25000, img: '../img2/5kg'},
+    {id: 1, nombre: "Matafuegos de 5kg", tipo: "5kg", cantidad: 1, desc: "Matafuegos para locales comerciales.", precio: 25000, img: '../img2/5kg.jpg'},
     {id: 2, nombre: "Matafuegos 1kg", tipo: "1kg", cantidad: 1, desc: "Matafuegos de 1kg ideales para automotores, y embarcaciones.", precio: 80000,  img: '../img2/1kg.jpg'},
     {id: 3, nombre: "Manguera", tipo: "N/A", cantidad: 1, desc: "Manguera de seguridad para sistemas de prevención.", precio: 1200,  img: '../img2/manguera.jpg'},
     {id: 4, nombre: "Cinta Doble Faz", tipo: "1U", cantidad: 1, desc: "Cinta doble faz para todo tipo de uso.", precio: 800,  img: '../img2/cintadoble.jpg'},
